a3e315d1207917f379022075e2f807897d0fdbb3
Laraib Khaild <laraibkhalid7861@gmail.com>
2021-02-28 16:26:21 +0500

    Add project files.

---
+++ CurrentWeatherApp/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CurrentWeatherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
